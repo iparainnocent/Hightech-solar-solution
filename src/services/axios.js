f392736086c6d4e144d313755063a6edfc9a4b99
innocent <iparainnocent@gmail.com>
2025-07-29 15:41:59 +0300

    Update API base URL to deployed Render backend

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://backend-1-xzp1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
